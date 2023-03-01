18a3cd13619cca5c4d15db2d4f0d53ed26817a18
163dgm <mgd361@nyu.edu>
2023-02-28 22:12:29 -0500

    Add prettier, eslint, and reorganize some files

---
+++ front-end/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:css/recommended',
+		'plugin:jsx-a11y/recommended',
+		'react-app',
+		'react-app/jest'
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['react', 'css', 'jsx-a11y'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'react/react-in-jsx-scope': 'off',
+		'prefer-const': 'error',
+		'no-unused-vars': 'error',
+		'no-var': 'error'
+	}
+}
