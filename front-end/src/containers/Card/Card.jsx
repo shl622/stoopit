ac721c37251d4aeb258dab2573c24db6a44cb92e
Cheng Charles Ji <szcharlesji@gmail.com>
2023-03-03 19:12:41 -0500

    removed tailwind-named classNames in css

--- front-end/src/containers/Card/Card.jsx
+++ front-end/src/containers/Card/Card.jsx
@@ -6,33 +6,31 @@ function Card(props) {
 	const { description, title, date, image } = props
 	return (
 		<>
-			<div className="card card-compact">
+			<div className="card card-compact card-layout">
 				<figure>
 					<img className="img" src={image} alt="stoop" />
 				</figure>
-				<div className="card-body">
+				<div className="card-body body">
 					<div className="text-stack">
 						<h2 className="title">{title}</h2>
 						<p className="text">{date}</p>
 						<p className="text">{description}</p>
 					</div>
 					{/* Heroicon: map */}
-					<div className="card-actions">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="icon"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z"
-							/>
-						</svg>
-					</div>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="icon"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z"
+						/>
+					</svg>
 				</div>
 			</div>
 		</>
