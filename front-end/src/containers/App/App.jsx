a50f42c96081477a16ddc49c1b9601641339e4d8
Cheng Charles Ji <szcharlesji@gmail.com>
2023-03-19 20:50:45 -0400

    solved conflict

--- front-end/src/containers/App/App.jsx
+++ front-end/src/containers/App/App.jsx
@@ -7,11 +7,7 @@ import MapPage from '../../routes/map/MapPage'
 import FeedPage from '../../routes/feed/FeedPage'
 import BottomNav from '../../components/BottomNav/BottomNav'
 import UploadPage from '../../routes/upload/UploadPage'
-<<<<<<< HEAD
-import SingleStoopMap from '../../components/Maps/SingleStoopMap/SingleStoopMap'
-=======
 import { MapProvider } from '../../context/map'
->>>>>>> master
 // import SubmitForm from '../../components/SubmitForm/SubmitForm'
 
 const App = () => {
@@ -22,23 +18,6 @@ const App = () => {
 	}, [])
 
 	return (
-<<<<<<< HEAD
-		<div className="app">
-			<main>
-				<Routes>
-					<Route path="/" element={<Navigate to="/feed" replace />} />
-					<Route path="/feed" element={<FeedPage />} />
-					<Route path="/upload" element={<UploadPage />} />
-					<Route
-						path="/map"
-						element={<MapPage currentPosition={currentPosition} />}
-					/>
-					<Route path="/map/:id" element={<SingleStoopMap />} />
-				</Routes>
-			</main>
-			<BottomNav />
-		</div>
-=======
 		<MapProvider
 			value={{
 				currentPosition,
@@ -66,7 +45,6 @@ const App = () => {
 				<BottomNav />
 			</div>
 		</MapProvider>
->>>>>>> master
 	)
 }
 
