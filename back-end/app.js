4fd70c7270aced6a58534bbf7b370e0d6e93b90c
szcharlesji <szcharlesji@gmail.com>
2023-03-26 16:46:48 -0400

    changed port number

--- back-end/app.js
+++ back-end/app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3000
+const port = 8080
 
 // listen on port 3000
 const listener = app.listen(port, () => {
