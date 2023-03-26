cb236a51709627375035b2b8d34c90333554b751
szcharlesji <szcharlesji@gmail.com>
2023-03-26 16:35:37 -0400

    test built front end

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,7 @@
 // import
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
 // listen on port 3000
@@ -12,6 +13,12 @@ const listener = app.listen(port, () => {
 const close = () => {
 	listener.close()
 }
+
+// Serve static files from the build folder
+app.use(express.static(path.join(__dirname, '../front-end/build')))
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../front-end/build/index.html'))
+})
 
 // export express app
 module.exports = app
