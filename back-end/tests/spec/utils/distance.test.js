671b0d60a95c88eae4bf0ddac0c7cc9b71814a4d
shl622 <shl622@nyu.edu>
2023-04-01 11:14:16 -0400

    feat: adds unit test for calculateDistance function and adds script for running test

---
+++ back-end/tests/spec/utils/distance.test.js
@@ -0,0 +1,12 @@
+const { calculateDistance } = require("../../../utils/distance")
+const { assert, should, expect } = require('chai')
+
+describe("Distance calculations", function (){
+    it("should return an error with undefined latitude and longitude values", function(){
+        expect(function(){calculateDistance()}).to.throw(Error,'test')    
+    })
+    it("should return return a number with valid latitude and longitude values", function(){
+        const result = calculateDistance(40.5,73.5,40.7,73.8)
+        assert.isNumber(result)
+    })
+})
