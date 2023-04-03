53d153e4a8b3ea735250aecb5d1712f0bf5cbe64
163dgm <mgd361@nyu.edu>
2023-04-03 01:53:33 -0400

    Integrate feed page with BE endpoint

--- front-end/src/routes/feed/FeedPage.jsx
+++ front-end/src/routes/feed/FeedPage.jsx
@@ -1,6 +1,5 @@
 import './FeedPage.css'
-import { useState } from 'react'
-import { Stoops } from '../../mockdata/db'
+import { useEffect, useState } from 'react'
 import Card from '../../components/Card/Card'
 import TopNav from '../../components/TopNav/TopNav'
 import { useContext } from 'react'
@@ -11,40 +10,73 @@ const FeedPage = () => {
 	const { currentPosition } = useContext(mapContext)
 	const [selectedRange, setSelectedRange] = useState(1)
 
-	const stoopsList = Stoops.map((stoop) => {
-		const distanceToStoop = calculateDistance(
-			currentPosition.lat,
-			currentPosition.lng,
-			stoop.location.lat,
-			stoop.location.lng
-		)
+	/**
+	 *  @typedef Stoop
+	 *  @property {number} id
+	 *	@property {{ lat: number, lng: number }} location
+	 *	@property {string} title,
+	 *	@property {number} timestamp UNIX Timestamp
+	 *	@property {string} image
+	 *	@property {string} description
+	 */
 
-		// Show card only if it is within selectedRange
-		return distanceToStoop <= selectedRange ? (
-			<Card
-				key={stoop.id}
-				id={stoop.id}
-				image={stoop.image}
-				title={stoop.title}
-				date={stoop.date}
-				lat={stoop.location.lat}
-				lng={stoop.location.lng}
-				description={stoop.description}
-			/>
-		) : (
-			<></>
-		)
-	})
+	const [stoops, setStoops] = useState([])
+
+	useEffect(() => {
+		if (currentPosition.lat && currentPosition.lng) {
+			fetch(
+				`http://localhost:8080/api/stoops?lat=${currentPosition.lat}&lng=${currentPosition.lng}&range=${selectedRange}`
+			)
+				.then((res) => res.json())
+				.then((res) => {
+					function sortbytime(a, b) {
+						return a.timestamp > b.timestamp
+							? -1
+							: a.timestamp < b.timestamp
+								? 1
+								: 0
+					}
+					res.data.sort(sortbytime)
+					setStoops(res.data)
+				})
+		}
+	}, [selectedRange, currentPosition.lat, currentPosition.lng])
 
 	return (
 		<>
 			<TopNav
 				currentPosition={currentPosition}
-				stoops={Stoops}
+				stoops={stoops}
 				selectedRange={selectedRange}
 				setSelectedRange={setSelectedRange}
 			/>
-			<div className="feed">{stoopsList}</div>
+			<div className="feed">
+				{stoops &&
+					stoops.map((/** @type {Stoop} */ stoop) => {
+						const distanceToStoop = calculateDistance(
+							currentPosition.lat,
+							currentPosition.lng,
+							stoop.location.lat,
+							stoop.location.lng
+						)
+
+						// Show card only if it is within selectedRange
+						return distanceToStoop <= selectedRange ? (
+							<Card
+								key={stoop.id}
+								id={stoop.id}
+								image={stoop.image}
+								title={stoop.title}
+								timestamp={stoop.timestamp}
+								lat={stoop.location.lat}
+								lng={stoop.location.lng}
+								description={stoop.description}
+							/>
+						) : (
+							<></>
+						)
+					})}
+			</div>
 		</>
 	)
 }
