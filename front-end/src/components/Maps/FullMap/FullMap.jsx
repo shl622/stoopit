b70e8256e5a60923cdb37ccfdbc83cc2e6e740bf
szcharlesji <szcharlesji@gmail.com>
2023-04-24 11:45:41 -0400

    added top bar in map page

--- front-end/src/components/Maps/FullMap/FullMap.jsx
+++ front-end/src/components/Maps/FullMap/FullMap.jsx
@@ -1,34 +1,66 @@
 import '../map.css'
 import { initMap, renderInitMarkers } from '../../../utils/map'
-import TopNav from '../../../components/TopNav/TopNav'
-import React, { useRef, useEffect } from 'react'
+import TopNav from '../../TopNav/TopNav'
+import { useContext } from 'react'
+import mapContext from '../../../context/map'
+import stoopContext from '../../../context/stoop'
+import { useState } from 'react'
 
 export default function FullMap({
-	selectedRange,
-	setSelectedRange,
 	center,
-	stoops
+	stoops,
+	selectedRange,
+	setSelectedRange
 }) {
 	const ref = useRef()
+	const { currentPosition } = useContext(mapContext)
+	const { setStoops } = useContext(stoopContext)
+	const [setLoading] = useState(true)
 
 	useEffect(() => {
 		const map = initMap({ stoops, ref, center })
 		renderInitMarkers({ stoops, map })
-	}, [center, stoops, selectedRange])
+	}, [center, stoops])
+
+	useEffect(() => {
+		if (currentPosition.lat && currentPosition.lng) {
+			fetch(
+				`http://localhost:8080/api/stoops?lat=${currentPosition.lat}&lng=${currentPosition.lng}&range=${selectedRange}`
+			)
+				.then((res) => res.json())
+				.then((res) => {
+					function sortbytime(a, b) {
+						if (a.timestamp > b.timestamp) {
+							return -1
+						} else if (a.timestamp < b.timestamp) {
+							return 1
+						} else {
+							return 0
+						}
+					}
+					res.data.sort(sortbytime)
+					setStoops(res.data)
+					setLoading(false)
+				})
+		}
+	}, [selectedRange, currentPosition.lat, currentPosition.lng])
 
 	return (
 		<>
-			<TopNav
-				stoops={stoops}
-				selectedRange={selectedRange}
-				setSelectedRange={setSelectedRange}
-			/>
-			<div
-				className="fullMap"
-				ref={ref}
-				id="map"
-				data-testid="full-map"
-			/>
+			<div className="fullMapContainer">
+				<TopNav
+					currentPosition={currentPosition}
+					stoops={stoops}
+					selectedRange={selectedRange}
+					setSelectedRange={setSelectedRange}
+				/>
+				<div
+					className="fullMap"
+					ref={ref}
+					id="map"
+					data-testid="full-map"
+				/>
+			</div>
 		</>
 	)
 }
